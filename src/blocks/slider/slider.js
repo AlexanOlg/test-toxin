1a23650cb489c80c405cbf639c833382c753a86c
Ellizevas <ellizevas@gmail.com>
2020-11-24 13:06:55 +0300

    Создание файла переменных variables.scss

---
+++ src/blocks/slider/slider.js
@@ -0,0 +1,12 @@
+import 'slick-carousel';
+
+$(() => {
+	$('.slider').slick({
+		dots: true,
+		waitForAnimate: false,
+		nextArrow:
+      '<button type="button" class="slick-next">keyboard_arrow_right</button>',
+		prevArrow:
+      '<button type="button" class="slick-prev">keyboard_arrow_left</button>',
+	});
+});
