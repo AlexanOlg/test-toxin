24286b57338770ef33c478bf5bafcbdb76d05847
Ellizevas <ellizevas@gmail.com>
2020-12-02 15:47:59 +0300

    Миксин like button
    Попробовала написать код, пока не получилось

---
+++ src/blocks/button/button.js
@@ -0,0 +1,16 @@
+// const $likeButtons = $('.button-like');
+
+// const handleClick = function currentTarget() {
+//	const $clickedBtn = $(currentTarget);
+//	const $likesCounter = $('.button-like__count', currentTarget);
+//	const likesCount = $likesCounter.text();
+
+//	$(currentTarget).toggleClass('button-like--clicked');
+
+//	const isClicked = $clickedBtn.hasClass('button-like--clicked');
+//	$likesCounter.text((isClicked) ? +likesCount + 1 : +likesCount - 1); // Increment or decrement likes
+
+//	$('.button-like__icon', currentTarget).text((isClicked) ? 'favorite' : 'favorite_border'); // Change icon
+// };
+
+// $likeButtons.click(handleClick);
