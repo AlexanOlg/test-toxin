383c4f16af5eea2ba1ee4a0fad66e29e1e078ac7
AlexanOlg <72731094+AlexanOlg@users.noreply.github.com>
2020-12-29 16:37:50 +0300

    form-elements под Pixel Perfect

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-	// Обработка клика по дропдауну
+	// Находим все дропдауны на странице
 	const dropdowns = document.querySelectorAll('.dropdown');
+
+	// Обработка клика по дропдауну
 	dropdowns.forEach((dropdown) => {
 		const button = dropdown.querySelector('.dropdown__button-tick');
 		const menu = dropdown.querySelector('.dropdown__menu');
 		button.addEventListener('click', () => menu.classList.toggle('active'));
 	});
 });
+// // Клик по кнопке применить - скрытие дропдауна
+// const buttonSimple = document.querySelectorAll('.button-simple');
+// const menu = document.querySelector('.dropdown__menu');
+// buttonSimple.forEach((listItem) => {
+// 	listItem.addEventListener('click', (event) => {
+// 		event.stopPropagation();
+// 		menu.classList.remove('active');
+// 	});
+// });
+// // Закрыть дропдаун по клику по странице
+// document.addEventListener('click', (event) => {
+// 	if (event.target !== dropdowns) {
+// 		menu.classList.remove('active');
+// 	}
+// });
