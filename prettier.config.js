898663f0c4095a2fe953bc4cb3bea7651a073777
AlexanOlg <72731094+AlexanOlg@users.noreply.github.com>
2021-03-16 10:30:07 +0300

    Updates

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	tabWidth: 4,
-	useTabs: true,
-	semi: true,
-	singleQuote: true,
-	trailingComma: 'all',
-	arrowParens: 'always',
-};
--- src/blocks/dropdown/dropdown-init.js
+++
@@ -1,7 +0,0 @@
-import { Dropdown } from './dropdown';
-
-function onLoad() {
-	document.querySelectorAll('.dropdown').forEach((item) => new Dropdown(item));
-}
-
-window.addEventListener('load', onLoad);
